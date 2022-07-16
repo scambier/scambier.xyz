b077da0361a6f294491c9999107fadbb2e721031
Simon Cambier <simon.cambier@protonmail.com>
2022-07-16 10:00:16 +0200

    Fixed static builds

--- src/components/ExternalLink.svelte
+++ src/components/ExternalLink.svelte
@@ -1,4 +1,4 @@
-<a {href} class="underline underline-offset-2"><slot /></a>
+<a {href} rel="external" class="underline underline-offset-2"><slot /></a>
 
 <script lang="ts">
     export let href: string;
